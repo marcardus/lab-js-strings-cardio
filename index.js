c9d3d678ccfdebc0749652ae743dd19e4051269b
marcardus <marcardus@gmail.com>
2025-03-29 11:55:16 +0100

    Solved lab

--- index.js
+++ index.js
@@ -5,8 +5,7 @@
 
 const string1 = "My favorite dessert is jello";
 
-// Your code here...
-
+console.log(string1.indexOf("j"));
 
 
 
@@ -17,8 +16,8 @@ const string1 = "My favorite dessert is jello";
 
 const string2 = "ABCDEFGHJKLO";
 
-// Your code here...
-
+const coolString = string2[2] + string2[10] + string2[10] + string2[11]; // C + O + O + L
+console.log(coolString);
 
 
 
@@ -29,8 +28,7 @@ const string2 = "ABCDEFGHJKLO";
 
 const string3 = "Na";
 
-// Your code here...
-
+console.log(string3.repeat(4) + " Batman!");
 
 
 
@@ -41,8 +39,8 @@ const string3 = "Na";
 
 const fruit = "banana apple mango orange lemon kiwi watermelon grapes pear pineapple";
 
-// Your code here...
-
+const favoriteFruit = fruit.slice(39, 49);
+console.log(favoriteFruit)
 
 
 /***************************************************
@@ -56,13 +54,19 @@ const funnyHeadline1 = "Breathing oxygen linked to staying alive";
 const funnyHeadline2 = "Students Cook & Serve Grandparents";
 
 
-// Check the first headline
-// Your code here ...
+if (funnyHeadline1.includes("oxygen")) {
+    console.log("The string includes the word 'oxygen'");
+  } else {
+    console.log("The string does not include the word 'oxygen'");
+  }
 
 
 // Check the second headline
-// Your code here ...
-
+if (funnyHeadline2.includes("oxygen")) {
+    console.log("The string includes the word 'oxygen'");
+  } else {
+    console.log("The string does not include the word 'oxygen'");
+  }
 
 
 /*******************************************
@@ -74,8 +78,8 @@ const string4 = "zEAWrTC9EgtxmK9w1";
 
 
 // a) Print the string length
-// Your code here ...
+console.log(string4.length);
 
 
 // b) Print the last character in the string
-// Your code here ...
+console.log(string4[string4.length - 1]);
